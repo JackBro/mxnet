9d52f47598cf68ab8a113c07887b334505a639a4
yajiedesign <yajiedesign@gmail.com>
2016-11-25 14:06:45 +0800

    add svm output gpu code add gpu test
    change gpu test

--- tests/python/gpu/test_operator_gpu.py
+++ tests/python/gpu/test_operator_gpu.py
@@ -1,5 +1,6 @@
 ﻿import sys
 import os
+
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
 sys.path.insert(0, os.path.join(curr_path, '../unittest'))
 from test_operator import *
@@ -13,6 +14,7 @@ set_default_context(mx.gpu(0))
 del test_support_vector_machine_l1_svm
 del test_support_vector_machine_l2_svm
 
+
 def test_batchnorm_with_type():
     sym = mx.sym.BatchNorm(name='norm', fix_gamma=False)
     ctx_list = [{'ctx': mx.gpu(0), 'norm_data': (10, 2, 10, 10), 'type_dict': {'norm_data': np.float32}},
@@ -22,8 +24,9 @@ def test_batchnorm_with_type():
     sym = mx.sym.BatchNorm(name='norm', fix_gamma=True)
     check_consistency(sym, ctx_list)
 
+
 def test_convolution_with_type():
-    sym = mx.sym.Convolution(num_filter=3, kernel=(3,3), name='conv')
+    sym = mx.sym.Convolution(num_filter=3, kernel=(3, 3), name='conv')
     ctx_list = [{'ctx': mx.gpu(0), 'conv_data': (2, 2, 10, 10), 'type_dict': {'conv_data': np.float64}},
                 {'ctx': mx.gpu(0), 'conv_data': (2, 2, 10, 10), 'type_dict': {'conv_data': np.float32}},
                 {'ctx': mx.gpu(0), 'conv_data': (2, 2, 10, 10), 'type_dict': {'conv_data': np.float16}},
@@ -31,14 +34,16 @@ def test_convolution_with_type():
                 {'ctx': mx.cpu(0), 'conv_data': (2, 2, 10, 10), 'type_dict': {'conv_data': np.float32}}]
     check_consistency(sym, ctx_list)
 
+
 def test_deconvolution_with_type():
-    sym = mx.sym.Deconvolution(num_filter=2, kernel=(3,3), name='deconv')
+    sym = mx.sym.Deconvolution(num_filter=2, kernel=(3, 3), name='deconv')
     ctx_list = [{'ctx': mx.gpu(0), 'deconv_data': (2, 2, 10, 10), 'type_dict': {'deconv_data': np.float64}},
                 {'ctx': mx.gpu(0), 'deconv_data': (2, 2, 10, 10), 'type_dict': {'deconv_data': np.float32}},
                 {'ctx': mx.gpu(0), 'deconv_data': (2, 2, 10, 10), 'type_dict': {'deconv_data': np.float16}},
                 {'ctx': mx.cpu(0), 'deconv_data': (2, 2, 10, 10), 'type_dict': {'deconv_data': np.float64}},
                 {'ctx': mx.cpu(0), 'deconv_data': (2, 2, 10, 10), 'type_dict': {'deconv_data': np.float32}}]
     check_consistency(sym, ctx_list)
+
 
 def test_pooling_with_type():
     ctx_list = [{'ctx': mx.gpu(0), 'pool_data': (10, 2, 10, 10), 'type_dict': {'pool_data': np.float64}},
@@ -47,26 +52,28 @@ def test_pooling_with_type():
                 {'ctx': mx.cpu(0), 'pool_data': (10, 2, 10, 10), 'type_dict': {'pool_data': np.float64}},
                 {'ctx': mx.cpu(0), 'pool_data': (10, 2, 10, 10), 'type_dict': {'pool_data': np.float32}}]
 
-    sym = mx.sym.Pooling(name='pool', kernel=(3,3), stride=(2,2), pool_type='max')
+    sym = mx.sym.Pooling(name='pool', kernel=(3, 3), stride=(2, 2), pool_type='max')
     check_consistency(sym, ctx_list)
 
-    sym = mx.sym.Pooling(name='pool', kernel=(3,3), pad=(1,1), pool_type='avg')
+    sym = mx.sym.Pooling(name='pool', kernel=(3, 3), pad=(1, 1), pool_type='avg')
     check_consistency(sym, ctx_list)
 
-    sym = mx.sym.Pooling(name='pool', kernel=(5,5), pad=(2,2), pool_type='max')
+    sym = mx.sym.Pooling(name='pool', kernel=(5, 5), pad=(2, 2), pool_type='max')
     check_consistency(sym, ctx_list)
 
-    sym = mx.sym.Pooling(name='pool', kernel=(3,3), pad=(1,1), pool_type='sum')
+    sym = mx.sym.Pooling(name='pool', kernel=(3, 3), pad=(1, 1), pool_type='sum')
     check_consistency(sym, ctx_list)
+
 
 def test_upsampling_with_type():
-    sym = mx.sym.UpSampling(scale=2, num_filter=2, name='up', sample_type = 'nearest', num_args=1)
+    sym = mx.sym.UpSampling(scale=2, num_filter=2, name='up', sample_type='nearest', num_args=1)
     ctx_list = [{'ctx': mx.gpu(0), 'up_arg0': (2, 2, 2, 10), 'type_dict': {'up_arg0': np.float64}},
                 {'ctx': mx.gpu(0), 'up_arg0': (2, 2, 2, 10), 'type_dict': {'up_arg0': np.float32}},
                 {'ctx': mx.gpu(0), 'up_arg0': (2, 2, 2, 10), 'type_dict': {'up_arg0': np.float16}},
                 {'ctx': mx.cpu(0), 'up_arg0': (2, 2, 2, 10), 'type_dict': {'up_arg0': np.float64}},
                 {'ctx': mx.cpu(0), 'up_arg0': (2, 2, 2, 10), 'type_dict': {'up_arg0': np.float32}}]
     check_consistency(sym, ctx_list)
+
 
 def test_concat_with_type():
     sym = mx.sym.Concat(name='concat', num_args=2)
@@ -81,6 +88,7 @@ def test_concat_with_type():
                 {'ctx': mx.cpu(0), 'concat_arg1': (2, 10), 'concat_arg0': (2, 10),
                  'type_dict': {'concat_arg0': np.float32, 'concat_arg1': np.float32}}]
     check_consistency(sym, ctx_list)
+
 
 def test_elementwisesum_with_type():
     sym = mx.sym.ElementWiseSum(name='ews', num_args=2)
@@ -98,13 +106,14 @@ def test_elementwisesum_with_type():
 
 
 def test_reshape_with_type():
-    sym = mx.sym.Reshape(name='reshape', shape=(-1,1,1,0))
+    sym = mx.sym.Reshape(name='reshape', shape=(-1, 1, 1, 0))
     ctx_list = [{'ctx': mx.gpu(0), 'reshape_data': (2, 2, 2, 10), 'type_dict': {'reshape_data': np.float64}},
                 {'ctx': mx.gpu(0), 'reshape_data': (2, 2, 2, 10), 'type_dict': {'reshape_data': np.float32}},
                 {'ctx': mx.gpu(0), 'reshape_data': (2, 2, 2, 10), 'type_dict': {'reshape_data': np.float16}},
                 {'ctx': mx.cpu(0), 'reshape_data': (2, 2, 2, 10), 'type_dict': {'reshape_data': np.float64}},
                 {'ctx': mx.cpu(0), 'reshape_data': (2, 2, 2, 10), 'type_dict': {'reshape_data': np.float32}}]
     check_consistency(sym, ctx_list)
+
 
 def test_blockgrad_with_type():
     sym = mx.sym.BlockGrad(name='bg')
@@ -115,6 +124,7 @@ def test_blockgrad_with_type():
                 {'ctx': mx.cpu(0), 'bg_data': (2, 2, 2, 10), 'type_dict': {'bg_data': np.float32}}]
     check_consistency(sym, ctx_list)
 
+
 def test_swapaxis_with_type():
     sym = mx.sym.SwapAxis(name='swap', dim1=1)
     ctx_list = [{'ctx': mx.gpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float64}},
@@ -124,6 +134,7 @@ def test_swapaxis_with_type():
                 {'ctx': mx.cpu(0), 'swap_data': (2, 2, 2, 10), 'type_dict': {'swap_data': np.float32}}]
     check_consistency(sym, ctx_list)
 
+
 def test_fullyconnected_with_type():
     sym = mx.sym.FullyConnected(num_hidden=3, name='inner')
     ctx_list = [{'ctx': mx.gpu(0), 'inner_data': (2, 10), 'type_dict': {'inner_data': np.float64}},
@@ -132,6 +143,7 @@ def test_fullyconnected_with_type():
                 {'ctx': mx.cpu(0), 'inner_data': (2, 10), 'type_dict': {'inner_data': np.float64}},
                 {'ctx': mx.cpu(0), 'inner_data': (2, 10), 'type_dict': {'inner_data': np.float32}}]
     check_consistency(sym, ctx_list)
+
 
 def test_activation_with_type():
     sym = mx.sym.Activation(name='act', act_type='sigmoid')
@@ -143,6 +155,7 @@ def test_activation_with_type():
                 {'ctx': mx.cpu(0), 'act_data': (2, 2, 10, 10), 'type_dict': {'act_data': np.float16}}]
     check_consistency(sym, ctx_list)
 
+
 def test_embedding_with_type():
     sym = mx.sym.Embedding(name='embedding', input_dim=10, output_dim=20)
     ctx_list = [{'ctx': mx.gpu(0), 'embedding_data': (2, 10), 'type_dict': {'embedding_data': np.float64}},
@@ -152,8 +165,20 @@ def test_embedding_with_type():
                 {'ctx': mx.cpu(0), 'embedding_data': (2, 10), 'type_dict': {'embedding_data': np.float32}},
                 {'ctx': mx.cpu(0), 'embedding_data': (2, 10), 'type_dict': {'embedding_data': np.float16}}]
     arg_params = {'embedding_data': np.random.randint(low=0, high=10, size=(2, 10))}
-    check_consistency(sym, ctx_list, grad_req={'embedding_data': 'null','embedding_weight': 'write'},
+    check_consistency(sym, ctx_list, grad_req={'embedding_data': 'null', 'embedding_weight': 'write'},
                       arg_params=arg_params)
+
+
+def test_svmoutput_with_type():
+    sym = mx.sym.SVMOutput(name='svmoutput', use_linear=True)
+    ctx_list = [{'ctx': mx.gpu(0), 'svmoutput_data': (20, 10), 'type_dict': {'svmoutput_data': np.float64}},
+                {'ctx': mx.gpu(0), 'svmoutput_data': (20, 10), 'type_dict': {'svmoutput_data': np.float32}},
+                {'ctx': mx.gpu(0), 'svmoutput_data': (20, 10), 'type_dict': {'svmoutput_data': np.float16}},
+                {'ctx': mx.cpu(0), 'svmoutput_data': (20, 10), 'type_dict': {'svmoutput_data': np.float64}},
+                {'ctx': mx.cpu(0), 'svmoutput_data': (20, 10), 'type_dict': {'svmoutput_data': np.float32}},
+                {'ctx': mx.cpu(0), 'svmoutput_data': (20, 10), 'type_dict': {'svmoutput_data': np.float16}}]
+    check_consistency(sym, ctx_list)
+
 
 if __name__ == '__main__':
     test_pooling_with_type()
@@ -169,6 +194,6 @@ if __name__ == '__main__':
     test_fullyconnected_with_type()
     test_activation_with_type()
     test_embedding_with_type()
-    #test_softmax_with_shape((3,4), mx.gpu())
-    #test_multi_softmax_with_shape((3,4,5), mx.gpu())
-
+    test_svmoutput_with_type()
+    # test_softmax_with_shape((3,4), mx.gpu())
+    # test_multi_softmax_with_shape((3,4,5), mx.gpu())
